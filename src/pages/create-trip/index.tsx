da72375dc4a102f42f92145d21c89af354dbaa2e
Michelle Cordeiro <mic321@gmail.com>
2024-07-11 21:59:57 -0300

    chore: add invite-guests-modal

--- src/pages/create-trip/index.tsx
+++ src/pages/create-trip/index.tsx
@@ -1,6 +1,7 @@
-import { MapPin, Calendar, ArrowRight, UserRoundPlus, Settings2, X, AtSign, Plus, User, Mail } from 'lucide-react'
+import { MapPin, Calendar, ArrowRight, UserRoundPlus, Settings2, X, User, Mail } from 'lucide-react'
 import { FormEvent, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { InviteGuestsModal } from './invite-guests-modal'
 
 export function CreateTripPage() {
   const navigate = useNavigate()
@@ -35,7 +36,7 @@ export function CreateTripPage() {
     setIsConfirmTripModalOpen(false)
   }
 
-  function addEmailToInvite(event: FormEvent<HTMLFormElement>) {
+  function addNewEmailToInvite(event: FormEvent<HTMLFormElement>) {
     event.preventDefault()
 
     const data = new FormData(event.currentTarget)
@@ -158,62 +159,12 @@ export function CreateTripPage() {
       </div>
 
       {isGuestsModalOpen && (
-        <div className='fixed inset-0 bg-black/60 flex items-center justify-center'>
-          <div className='w-[640px] rounded-xl px-6 py-5 shadow-shape bg-zinc-900 space-y-5'>
-            <div className='space-y-2'>
-              <div className='flex items-center justify-between'>
-                <h2 className='text-lg font-semibold'>Selecionar convidados</h2>
-                <button type='button' onClick={closeGuestsModal}>
-                  <X className='size-5 text-zinc-400' />
-                </button>
-              </div>
-
-              <p className='text-sm text-zinc-400'>
-                Os convidados irão receber e-mails para confirmar a participação na viagem.
-              </p>
-            </div>
-
-            <div className='flex flex-wrap gap-2'>
-              {emailsToInvite.map(email => {
-                return (
-                  <div className='px-2.5 py-1.5 rounded-md bg-zinc-800 flex items-center gap-2'>
-                    <span key={email} className='text-zinc-300'>
-                      {email}
-                    </span>
-                    <button type='button' onClick={() => removeEmailFromInvites(email)}>
-                      <X className='size-4 text-zinc-400' />
-                    </button>
-                  </div>
-                )
-              })}
-            </div>
-
-            <div className='w-full h-px bg-zinc-800' />
-
-            <form
-              onSubmit={addEmailToInvite}
-              className='p-2.5 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2'
-            >
-              <div className='px-2 flex items-center flex-1 gap-2 '>
-                <AtSign className='text-zinc-400 size-5' />
-                <input
-                  type='email'
-                  name='email'
-                  placeholder='Digite o e-mail do convidado'
-                  className='bg-transparent text-lg placeholder-zinc-400 w-40 outline-none flex-1'
-                />
-              </div>
-
-              <button
-                type='submit'
-                className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400'
-              >
-                Convidar
-                <Plus className='size-5' />
-              </button>
-            </form>
-          </div>
-        </div>
+        <InviteGuestsModal
+          emailsToInvite={emailsToInvite}
+          addNewEmailToInvite={addNewEmailToInvite}
+          closeGuestsModal={closeGuestsModal}
+          removeEmailFromInvites={removeEmailFromInvites}
+        />
       )}
 
       {isConfirmTripModalOpen && (
@@ -237,7 +188,7 @@ export function CreateTripPage() {
 
             <div className='flex flex-wrap gap-2'></div>
 
-            <form onSubmit={addEmailToInvite} className='space-y-3'>
+            <form onSubmit={addNewEmailToInvite} className='space-y-3'>
               <div className='px-4 h-14 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2'>
                 <User className='text-zinc-400 size-5' />
                 <input
